465da79aef7a96ef1ba29a1c8bfe9139adca73dc
sads4ndwich <caiocamargo007@gmail.com>
2023-06-09 10:20:52 -0300

    fix: get username from cookie and include into new post sql insertion command

--- process/newpost-process.php
+++ process/newpost-process.php
@@ -7,9 +7,10 @@ if (count($_FILES) > 0) {
         $conn = connectDatabase();
 
         $imgData = base64_encode(file_get_contents(addslashes($_FILES['file']['tmp_name'])));
+        $username = $_COOKIE['username'];
         $title = $_POST['title'];
         $description = $_POST['description'];
-        $sql = "insert into posts (user, title, description, image) values ('sad', '$title', '$description', '$imgData')";
+        $sql = "insert into posts (user, title, description, image) values ('$username', '$title', '$description', '$imgData')";
 
         mysqli_query($conn, $sql);
 
