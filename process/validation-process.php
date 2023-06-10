4743372da1142d8c80d18b4e5cf11197185855a6
sads4ndwich <caiocamargo007@gmail.com>
2023-06-10 17:22:25 -0300

    feat(validation): added authenticated and unauthenticated guard functions

--- process/validation-process.php
+++ process/validation-process.php
@@ -1,7 +1,14 @@
 <?php
-    function validarLogin(){
+    function onlyAuthenticatedUser(){
         if(!isset($_COOKIE['username'])){
             header("Location:../views/login.php");
+            die();
+        }
+    }
+
+    function onlyUnauthenticatedUser() {
+        if(isset($_COOKIE['username'])){
+            header("Location:../views/home.php");
             die();
         }
     }
