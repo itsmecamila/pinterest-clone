3542c4711fa321d050e996b6ba11a5e962189826
sads4ndwich <caiocamargo007@gmail.com>
2023-06-10 17:20:57 -0300

    feat(comment): include create comment function

--- daos/comment.php
+++ daos/comment.php
@@ -1,6 +1,13 @@
 <?php
     require_once "../services/db.php";
 
+    function createComment($postId, $userId, $comment) {
+        $conn = connectDatabase();
+        $createCommentSqlQuery = "insert into comments (post_id,user_id,comment) values ('$postId','$userId','$comment')";
+
+        mysqli_query($conn,$createCommentSqlQuery);
+    }
+
     function getCommentsFromPost($post_id) {
         $conn = connectDatabase();
         $getCommentsFromPostSqlQuery = "select * from comments where post_id ='$post_id'";
