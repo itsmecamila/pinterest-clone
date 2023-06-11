3970d9448f82aca1cad28cdc026647cb4b17b08e
sads4ndwich <caiocamargo007@gmail.com>
2023-06-11 17:00:59 -0300

    feat(post): added download image button

--- views/post.php
+++ views/post.php
@@ -77,7 +77,10 @@
             <div class="post-details"> <!--Informações do post-->
                 <header><!--Botão para SALVAR post-->
                 <?php
-                    echo '<a href="../views/savepost.php?post_id='.$post['id'].'">Salvar</a>';
+                    echo '<a class="download-post" href="data:image/png;base64,' . $post['image'] . '" download>';
+                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#000" d="M6 20q-.825 0-1.413-.588T4 18v-3h2v3h12v-3h2v3q0 .825-.588 1.413T18 20H6Zm6-4l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11l-5 5Z"/></svg>';
+                    echo '</a>';
+                    echo '<a class="save-post" href="../views/savepost.php?post_id='.$post['id'].'">Salvar</a>';
                 ?>    
                     
                 </header>
